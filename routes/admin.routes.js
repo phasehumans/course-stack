451c20f8c7551ce259901e198059e0cbdcfeb473
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-11-02 15:26:10 +0530

    users controllers

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,21 @@
 const {Router} = require('express')
 const adminRouter= Router()
 const {AdminModel} = require('../db')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+
+function authMiddleware(req, res, next){
+    const token = req.headers.token
+    const decodedData= jwt.verify(token, process.env.JWT_SECRET)
+
+    if(decodedData){
+        
+    }
+
+}
 
 adminRouter.post('/signup', (req, res) => {
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,47 @@
 const {Router} = require('express')
 const userRouter = Router()
+const {z} = require('zod')
+const bcrypt = require('bcrypt')
+const {UserModel} = require('../db')
 
 
-userRouter.post('/signup', (req, res) => {
+userRouter.post('/signup', async (req, res) => {
+    const requiredBody = z.object({
+        email : z.email().string(),
+        password : z.string().string().min(5).max(20),
+        firstName : z.string(),
+        lastName : z.string()
+    })
+
+    const parsedData = requiredBody.safeParse(req.body)
+    if(!parsedData.success){
+        res.json({
+            message: "invalid input",
+            err: parsedData.error
+        })
+    }
+
+    const email = req.body.email
+    const password = req.body.password
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+
+
+    try {
+        const hashPassword = await bcrypt.hash(password, 5)
+        await UserModel.create({
+            firstName : firstName,
+            lastName : lastName,
+            email : email,
+            password : hashPassword
+        })
+    } catch (error) {
+        
+    }
+
+    res.json({
+        message : "you are logged in"
+    })
 
 })
 
