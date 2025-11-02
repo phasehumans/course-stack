0108b7a9d3ace893260d052c320e4d441e5b76bf
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-11-02 10:56:33 +0530

    define routing

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
 const express = require('express')
-
+const {userRouter} = require('./routes/user.routes')
+const { adminRouter } = require('./routes/admin.routes')
+const { courseRouter } = require('./routes/course.routes')
 
 const app = express()
-
 app.use(express.json())
 
-app.post('/user/signup', (req, res) => {
+// routes
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/admin', adminRouter)
+app.use('/api/v1/course', courseRouter)
 
-})
-
-app.post('/user/signin', (req, res) => {
-
-})
-
-app.get('/user/purchases', (req, res) => {
-
-})
-
-app.post("/course/purchase", (req, res) => {
-
-});
-
-app.get('/course/preview', (req, res) => {
-
-})
-
-app.post('/admin/signup', (req, res) => {
-
-})
-
-app.post('/admin/createcourse', (req, res) => {
-
-})
-
-app.delete('/admin/deletecourse', (req, res) => {
-
-})
-
-app.post('/admin/addcontent', (req, res) => {
-
-})
 
 app.listen(3000)
